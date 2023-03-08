ea55682c7e473c54bf6b6b30f55f4d4d16263d1e
Vadim Artyushenko <artushenkomail@mail.ru>
2023-03-08 23:33:18 +0700

    Add project files.

---
+++ ConsoleAppWithIoC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleAppWithIoC
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = CreateConfigurationBuilder(args).Build();
+            Console.WriteLine("Hello, World!");
+        }
+
+        private static IConfigurationBuilder CreateConfigurationBuilder(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+
+            return builder
+                .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty)
+                .AddJsonFile("appSettings.json", false, false)
+                .AddEnvironmentVariables();
+        }
+    }
+}
